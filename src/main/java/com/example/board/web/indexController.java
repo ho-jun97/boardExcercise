144b07910d5311a6fe501952f60b56e4cc0c1845
ho-jun97 <hojun9707@naver.com>
2022-06-03 14:21:10 +0900

    feat : create main UI

--- src/main/java/com/example/board/web/indexController.java
+++ src/main/java/com/example/board/web/indexController.java
@@ -1,12 +1,9 @@
 package com.example.board.web;
 
-import com.example.board.domain.user.User;
-import com.example.board.service.UserService;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RequiredArgsConstructor
 @Controller
